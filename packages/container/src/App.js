19d80936823f8e22aefc5afb04af6619f7a72055
Zaid Ahmed <zaid.ahmed@techwards.co>
2021-10-04 12:17:34 +0500

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from './components/MarketingApp'
 
 export default () => {
 return (
+    <BrowserRouter>
     <div>
-        <h1>Hi there!!!!!!!!!!</h1> 
-         <hr/> 
+        <Header/>
           {/* Some after 10-15 years we want to change the container to some other frameWork 
           like Angular or something else, We need near zero coupling between container and marketing app,
           So if we want to change the container there is no need to change in marketing app, that why we need to
           wrap the mount function into component here!  */}
         <MarketingApp/>
     </div>
+    </BrowserRouter>
  );
 };
