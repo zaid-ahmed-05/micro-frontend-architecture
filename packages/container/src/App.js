64d05d6be4dd319c0db985e3ef63dbf0dbb13b19
Zaid Ahmed <zaid.ahmed@techwards.co>
2021-10-04 12:58:52 +0500

    Randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider,createGenerateClassName } from "@material-ui/core/styles";
 import Header from "./components/Header";
 import MarketingApp from './components/MarketingApp'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix:'co',
+});
 
 export default () => {
 return (
     <BrowserRouter>
-    <div>
-        <Header/>
-          {/* Some after 10-15 years we want to change the container to some other frameWork 
-          like Angular or something else, We need near zero coupling between container and marketing app,
-          So if we want to change the container there is no need to change in marketing app, that why we need to
-          wrap the mount function into component here!  */}
-        <MarketingApp/>
-    </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                 <Header/>
+                {/* Some after 10-15 years we want to change the container to some other frameWork 
+                like Angular or something else, We need near zero coupling between container and marketing app,
+                So if we want to change the container there is no need to change in marketing app, that why we need to
+                wrap the mount function into component here!  */}
+                 <MarketingApp/>
+            </div>
+        </StylesProvider>
     </BrowserRouter>
  );
 };
