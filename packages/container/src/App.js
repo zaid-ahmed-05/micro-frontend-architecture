69a1c75efd029f480cd64384fbe861ee31b9b897
Zaid Ahmed <zaid.ahmed@techwards.co>
2021-10-04 11:20:22 +0500

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
 return (
     <div>
-        <h1>Hi there !@ @@</h1> 
+        <h1>Hi there!!!!</h1> 
          <hr/> 
           {/* Some after 10-15 years we want to change the container to some other frameWork 
           like Angular or something else, We need near zero coupling between container and marketing app,
